12af1393dc13534a5dc07fbcc13d779fec3fab17
faizan-adil1 <adilmuhammadfaizan@gmail.com>
2024-12-27 05:14:54 +0500

    changes

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -27,53 +27,12 @@ export default function Form() {
   const sendEmail = (params) => {
      const toastId = toast.loading("Sending your message, please wait...");
 
-    // toast.info(
-    //   "Form submissions are demo-only here. Please checkout the final code repo to enable it. If you want to connect you can reach out to me via adilmuhammadfaizan@gmail.com.",
-    //   {
-    //     id: toastId,
-    //   }
-    // );
-
-    // comment out the above toast.info and uncomment the below code to enable emailjs
-
-    // emailjs
-    //   .send(
-    //     process.env.NEXT_PUBLIC_SERVICE_ID,
-    //     process.env.NEXT_PUBLIC_TEMPLATE_ID,
-    //     params,
-    //     {
-    //       publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
-    //       limitRate: {
-    //         throttle: 5000, // you can not send more then 1 email per 5 seconds
-    //       },
-    //     }
-    //   )
-    //   .then(
-    //     () => {
-    //       toast.success(
-    //         "I have received your message, I will get back to you soon!",
-    //         {
-    //           id: toastId,
-    //         }
-    //       );
-    //     },
-    //     (error) => {
-    //       console.log("FAILED...", error.text);
-    //       toast.error(
-    //         "There was an error sending your message, please try again later!",
-    //         {
-    //           id: toastId,
-    //         }
-    //       );
-    //     }
-    //   );
-
     emailjs
     .send(
       process.env.NEXT_PUBLIC_SERVICE_ID,
       process.env.NEXT_PUBLIC_TEMPLATE_ID,
       params,
-      process.env.NEXT_PUBLIC_PUBLIC_KEY // Pass public key directly here
+      process.env.NEXT_PUBLIC_PUBLIC_KEY
     )
     .then(
       () => {
@@ -169,7 +128,7 @@ export default function Form() {
         <motion.input
           variants={item}
           value="Cast your message!"
-          className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid
+          className="w-full px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid
                      hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize"
           type="submit"
         />
